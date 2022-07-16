9755f4fa72c4cfe2fda7b1337ed717d1c49cb798
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-07-16 15:19:59 +0300

    commit tree e990385e01d6a1ae130cff63378c9bb3b1fa2b80

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
+
+// In the Item component, when the user clicks the <button> element, the item should be added to their virtual cart. The way we'll show the user that the item is in the cart is by changing the className on the <li> element:
+
+// <li className="in-cart">
+// {/* the item is in the cart */}
+// </li>
+
+// <li className="">
+// {/* the item is NOT in the cart */}
+// </li>
+// If the item is not in the cart, the <button> element's text should read "Add to Cart", and if the item is in the cart, the <button> element's text should read "Remove From Cart". Naturally, you'll also need to add state to the Item component to solve this deliverable!
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  function cartItem(){
+    setInCart((inCart) => !inCart)
+  }
+
   return (
-    <li className="">
+    <li className={inCart ? "in-cart": ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={cartItem}>{inCart ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
